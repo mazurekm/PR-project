8b2d622b5b5e763c12e1c2a3ecbba6f56045891e
Michał <mazur@dhcppc5.home>
2013-12-17 14:42:14 +0100

    some changes

--- first_method.cpp
+++ first_method.cpp
@@ -2,36 +2,73 @@
 #include<cmath>
 #include<omp.h>
 
-const int N=10000000;
-const int S=sqrt(N);
+typedef unsigned long uL;
 
+const uL N=100;
+const uL S=(int)sqrt(N);
 
-int main(int argc,char **argv) {
-	long a[S+1];
-	long primes[N/10];
-	long p_num_count=0,k=0,rest=0;
+uL primes1[N];
+uL primes2[N];
+uL p_num_count=0;
 
-	#pragma omp parallel for default(none) shared(a)
-	for(long i=2;i<=S;++i) a[i]=1;
-	for(long i=2;i<=S;++i) { //sito wyznacza dzielniki
+void genDividers() {
+	uL a[S+1];
+	#pragma omp paralell for default(none) shared(a) //tylko do pierwszego for'a
+	for(uL i=2;i<=S;++i) a[i]=1;
+	for(uL i=2;i<=S;++i) { //sito wyznacza dzielniki
 		if(a[i]==1) {
-			primes[p_num_count++]=i;
-			for(k=i+1;k<=S;k+=i) a[k]=0;
+			primes1[p_num_count]=i;
+			primes2[p_num_count]=i;
+			++p_num_count;
+			for(uL k=i+i;k<=S;k+=i) a[k]=0;
 		}
 	}
+}
 
-	long deviders=p_num_count;
-
-	#pragma omp parallel for default(none) private(k,rest) shared(primes,p_num_count,deviders)
-	for(long i=S+1;i<=N;++i){
+void sequiental() {
+	uL rest,i,k,deviders=p_num_count;
+	uL count=p_num_count;
+	double start=omp_get_wtime();
+	for(uL i=S+1;i<=N;++i){
 		for(k=0;k<deviders;++k){
-			rest=(i%primes[k]);
+			rest=(i%primes2[k]);
+			if(!rest) break;
+		}
+		if(rest) {
+			primes2[count++]=i;
+		}
+	}
+	double end=omp_get_wtime();
+	printf("\n sequiental %lf \n",end-start);
+}
+
+void parallel() {
+	double start=omp_get_wtime();
+	uL rest,i,k,deviders=p_num_count;
+	uL count=p_num_count;
+	#pragma omp parallel for default(none) private(rest,k) shared(primes1,count,deviders)
+	for(uL i=S+1;i<=N;++i){
+		for(k=0;k<deviders;++k){
+			rest=(i%primes1[k]);
 			if(!rest) break;
 		}
 		if(rest) {
 			#pragma omp critical
-			primes[p_num_count++]=i;
+			primes1[count++]=i;
 		}
 	}
+	double end=omp_get_wtime();
+	printf("\n parallel %lf \n",end-start);
+	for(uL i=0;i<count;++i)
+			printf("%lu ",primes1[i]);
+}
+
+
+
+
+int main(int argc,char **argv) {
+	genDividers();
+	sequiental();
+	parallel();
 	return 0;
 }
--- second_method.cpp
+++ second_method.cpp
@@ -1,75 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
 #include <omp.h>
-#include <math.h>
-#include <time.h>
-#include <sys/time.h>
-#include <vector>
+#include <cmath>
+
+
+typedef unsigned long uL;
+const int max_value=100;
+
 
 /* sieve of Eratosthenes - sequelntial */
 
-void sieve_sequential(unsigned long max_value) {
-	//init table - clean
-	bool *tab = (bool*) calloc (max_value, sizeof(bool));
-
-double start = omp_get_wtime();
-
-	//deleting multiples
-	for (unsigned long i=2; i<max_value; ++i) {
+void sieve_sequential() {
+	bool tab[max_value+1];
+	for(uL i=0;i<=max_value;++i) tab[i]=false;
+	double start = omp_get_wtime();
+	for (uL i=2; i<=max_value; ++i) {
 		if(tab[i] == false) { 
-			//printf("%ld ", i);
-			unsigned long j = i;
-			while(j+i < max_value) {
-				j = j + i;
-				tab[j] = 1;
-			}
+			for(uL j=i+i;j<= max_value;j+=i)
+				tab[j] = true;
 		}
 	}
-        printf("\nSieve of Eratosthenes - sequelntial: %f\n", omp_get_wtime() - start);
-
-	/*for (unsigned long i=2; i<max_value; ++i)
-		if(tab[i] == false)
-			printf("%lu ", i);
-	printf("\n");*/
-
+	double end = omp_get_wtime();
+	printf("\nSieve of Eratosthenes - sequelntial: %f\n", end - start);
 }
-
 
 /* sieve of Eratosthenes - parallel */
 
-void sieve_parallel(unsigned long max_value) {
-	//init table - clean
-	bool *tab = (bool*) calloc (max_value, sizeof(bool));
-	//deleting multiples
-	unsigned long i, j;
-	//omp_set_num_threads(4);
+void sieve_parallel() {
+	uL i,p_num_count;
+	bool tab[max_value+1];
+	for(i=0;i<=max_value;++i) tab[i]=false;
 	double start = omp_get_wtime();
-	#pragma omp parallel default(none) shared(tab,max_value) private(i,j)
+	#pragma omp parallel default(none) shared(tab) private(i)
 	{
 		#pragma omp for
-		for (i=2; i<max_value; ++i) {
-			if(tab[i] == false) { 
-				//printf("%ld ", i);
-				for(j=i+i;j<max_value;j+=i) {
+		for (i=2; i<=max_value; ++i) {
+			if(tab[i] == false) {
+				for(uL j=i+i;j<=max_value;j+=i) {
 					tab[j] = true;
 				}
 			}
 		}
 	}
-
-        printf("\nSieve of Eratosthenes - parallel: %f\n", omp_get_wtime() - start);
-
-	for (unsigned long i=2; i<max_value; ++i)
-		if(tab[i] == false)
-			printf("%lu ", i);
-	printf("\n");
-
+	double end =omp_get_wtime();
+	printf("\nSieve of Eratosthenes - parallel: %f\n", end - start);
+	for(i=2;i<=max_value;++i)
+			if(!tab[i])
+				printf("%lu ",i);
 }
 
 
 int main(int argc, char **argv) {
-	//omp_set_num_threads(3);
-	sieve_sequential(100000);
-	sieve_parallel(100000);
+	sieve_sequential();
+	sieve_parallel();
 	return 0;
 }
