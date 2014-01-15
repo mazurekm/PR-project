662b921b33f16cbaf394ea1094357b2bfacd4634
Michał <mazur@dhcppc5.home>
2014-01-15 15:14:49 +0100

    single cache access

--- second_method.cpp
+++ second_method.cpp
@@ -1,11 +1,13 @@
-
 #include <cstdio>
 #include <omp.h>
 #include <cmath>
-//#include <Windows.h>
+#include <fstream>
+
+using namespace std;
 
 typedef unsigned long uL;
 const int max_value=40000000;
+
 
 
 /* sieve of Eratosthenes - sequelntial */
@@ -51,32 +53,34 @@ double sieve_parallel() {
 //jednokrotny pobieranie do pamięci podręcznej
 
 double sieve_parallel_v() {
-	long i;
+		uL i;
         uL p_num_count;
 		const uL cache=6*1024*1024;
-		uL crange=max_value*sizeof(bool)/cache;
+		int n=sqrt((double) max_value);
         bool * tab = new bool[max_value+1];
         for(i=0;i<=max_value;++i) tab[i]=false;
         double start = omp_get_wtime();
 		
-		for(uL k=0;k<crange;++k) {
-        #pragma omp parallel default(none) shared(tab) private(i,crange,k)
+		for(uL k=0;k<max_value+cache;k+=cache) {
+        #pragma omp parallel default(none) shared(tab,n,k) private(i)
         {
-				uL n=0;
-				if((k+1)*cache>max_value)
-					n=max_value;
-				else
-					n=(k+1)*cache;
                 #pragma omp for schedule(static, 2)
-                	for (i=k*cache; i<=n; ++i) {
+                	for (i=2; i<=n; ++i) {
                     	    if(tab[i] == false) {
-                        	        for(uL j=i+i;j<=n;j+=i) {
+									uL j=0;
+									if(k!=0) 
+										j=k+(i-k%i);
+									else 
+										j=i+i;
+                        	        for(;j<=max_value&&j<=k+cache;j+=i) {
                             	            tab[j] = true;
                                	 }
                         	}
                 	}
 		}
         }
+		for(int i=cache;i<2*cache;++i)
+				if(!tab[i]) printf("%d ",i);
 		delete [] tab;
         return omp_get_wtime() - start;
        
@@ -84,8 +88,12 @@ double sieve_parallel_v() {
 
 
 int main(int argc, char **argv) {
+		fstream fd; 
+		fd.open("time.txt",fstream::in|fstream::out);
 		//printf("%f\n", sieve_sequential());
-		printf("%f\n", sieve_parallel());
-		printf("%f\n", sieve_parallel_v());
+		//printf("%f\n", sieve_parallel());
+		//printf("%f\n", sieve_parallel_v());
+		fd <<sieve_parallel_v() <<endl;
+		fd.close();
         return 0;
 }
